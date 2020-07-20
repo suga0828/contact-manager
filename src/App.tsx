c2a9c1f51a4f991212059cee535eb567c7ce18c9
Alexander Sandoval <alexander.sf0828@gmail.com>
2020-07-20 16:18:35 -0500

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useReducer } from 'react';
 
-import './App.css';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/layout/Header';
@@ -9,6 +7,7 @@ import Header from './components/layout/Header';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import { mockedContacts, reducer, ContactInfo, ContactActions } from './components/contacts/Contacts.reducer';
 
@@ -21,9 +20,10 @@ const App = (): JSX.Element => {
       <Header brand="Contact Manager" />
       <div className="w-11/12 sm:w-3/4 max-w-screen-lg mx-auto my-10">
         <Switch>
-          <Route path="/about"><About /></Route>
-          <Route path="/contact/add"><AddContact addHandler={(contact: ContactInfo) => dispatch({ type: ContactActions.add, payload: contact })} /></Route>
-          <Route path="/"><Contacts data={contacts} deleteHandler={(id: number) => dispatch({ type: ContactActions.delete, payload: id })} /></Route>
+          <Route exact path="/"><Contacts data={contacts} deleteHandler={(id: number) => dispatch({ type: ContactActions.delete, payload: id })} /></Route>
+          <Route exact path="/about"><About /></Route>
+          <Route exact path="/contact/add"><AddContact addHandler={(contact: ContactInfo) => dispatch({ type: ContactActions.add, payload: contact })} /></Route>
+          <Route><NotFound /></Route>
         </Switch>
       </div>
     </Router>
---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <h2 className="font-mono text-5xl mb-6"><span className="text-red-600">404</span> Page Not Found</h2>
+      <p>Sorry, that page does not exist.</p>
+    </>
+  )
+}
+
+export default NotFound;
